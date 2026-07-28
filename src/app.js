770267c1d051e4c68693d8b34a7c1f920daad36b
osndl.rinkug <osndl.rinkug@aicofindia.com>
2026-07-28 14:15:05 +0530

    Added home route

--- src/app.js
+++ src/app.js
@@ -9,6 +9,14 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+
+app.get("/", (req, res) => {
+    res.json({
+      success: true,
+      message: "NextChat Backend is Running 🚀"
+    });
+  });
+
 // test routes
 app.use("/api", authRoute);
 
