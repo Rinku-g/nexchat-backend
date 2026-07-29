483db5c8c79fbeafe9254f0c447757bd84939f9c
osndl.rinkug <osndl.rinkug@aicofindia.com>
2026-07-29 10:59:03 +0530

    add gitingnore file

--- src/app.js
+++ src/app.js
@@ -4,20 +4,17 @@ import authRoute from "./routes/index.js";
 
 const app = express();
 
-// middleware
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-
 app.get("/", (req, res) => {
-    res.json({
-      success: true,
-      message: "NextChat Backend is Running 🚀"
-    });
+  res.json({
+    success: true,
+    message: "NextChat Backend is Running 🚀",
   });
+});
 
-// test routes
 app.use("/api", authRoute);
 
 export default app;
